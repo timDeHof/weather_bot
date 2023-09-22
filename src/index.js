db14ad0737a8302dcd5228fbf16cca73a65714fa
timDeHof <this.is.tim.dehof@gmail.com>
2023-09-22 11:51:40 -0400

    chore: add .gitignore, package.json, and initial files for Discord bot The commit adds the following changes: - Added .gitignore file to exclude node_modules/ and .env from version control. - Added package.json file with basic project information and dependencies. - Added initial files for the Discord bot: ping.js and index.js. These changes are done to set up the project structure and dependencies for a Discord bot.

---
+++ src/index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+client.on(Events.ClientReady, () => {
+  console.log("Logged in!");
+});
+
+client.login();
